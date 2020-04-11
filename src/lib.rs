c48213ad706e9d14117369027338dc81395236a7
Damien <damien@sail>
2020-04-11 21:09:53 +0930

    Exercise 01-03

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod tests {
+  use std::collections::BTreeMap;
+
   static HEX_CHARS: &str = "0123456789abcdef";
 
   fn hex_to_byte(first: char, second: char) -> u8 {
@@ -64,7 +66,20 @@ mod tests {
       let b2 = i2.next().unwrap();
       r.push(b1 ^ b2);
     }
+
+    r
+  }
+
+  fn freq<T>(data: Vec<T>) -> BTreeMap<T, u32>
+  where T: Ord {
+    let mut r: BTreeMap<T, u32> = BTreeMap::new();
   
+    for c in data {
+      r.entry(c)
+        .and_modify(|n| *n += 1 )
+        .or_insert(1);
+    }
+
     r
   }
 
@@ -85,4 +100,27 @@ mod tests {
     assert_eq!(read_hex("746865206b696420646f6e277420706c6179"), xor(binary, xor_value));
   }
 
+  #[test]
+  fn test_01_03_xor_single() {
+    let src = read_hex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+
+    let mut answer = String::new();
+
+    for x in 0..=255 {
+      let xor : Vec<u8> = src.iter().map(|n| n ^ x).collect();
+      let freqs : BTreeMap<u8, u32> = freq(xor.clone());
+      let mut freq_in_order : Vec<u8> = freqs.keys().cloned().collect();
+      // Look for the top 3 letters in some common ones
+      freq_in_order.sort_by(|a, b| freqs.get(b).unwrap().partial_cmp(freqs.get(a).unwrap()).unwrap());
+      freq_in_order.truncate(3);
+      if freq_in_order.iter().all(|x| b" etarsno".contains(x)) {
+        answer.insert_str(0, std::str::from_utf8(&xor).unwrap());
+        break;
+      }
+    }
+
+    assert_eq!("Cooking MC\'s like a pound of bacon", answer);
+  }
+
 }
+
